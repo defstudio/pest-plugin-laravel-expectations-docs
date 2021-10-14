2b195a6ac941d4af8eba0ba83a4ef4889e62d2bb
fabio-ivona <fabio-ivona@users.noreply.github.com>
2021-10-14 17:42:14 +0000

    deploy: 78b832f92e448aa9dc8e37f3e829c8453a2d6365

---
+++ _nuxt/static/1634233333/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002Freleases","\u002F","\u002Fstart\u002FInstallation-%26-Configuration","\u002Fusage\u002FAutocompletion","\u002Fusage\u002FExpectations"]})
